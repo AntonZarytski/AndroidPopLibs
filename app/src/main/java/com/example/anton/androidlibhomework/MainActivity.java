9c378b0d387ba462e8b6a195e05189491310057b
Venom <antonzaritsky@gmail.com>
2018-05-30 21:28:09 +0300

    working with homework3

--- app/src/main/java/com/example/anton/androidlibhomework/MainActivity.java
+++ app/src/main/java/com/example/anton/androidlibhomework/MainActivity.java
@@ -1,13 +1,89 @@
 package com.example.anton.androidlibhomework;
 
-import android.support.v7.app.AppCompatActivity;
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+    private static final int LOADED_PHOTO = 345;
+    private static final int READ_STORAGE = 467;
+    @BindView(R.id.choose_photo_btn)
+    Button choosePhotoBtn;
+    @BindView(R.id.choosen_photo_iv)
+    ImageView choosenPhoto;
+    @BindView(R.id.convert_to_png_btn)
+    Button convertBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.choose_photo_btn)
+    public void choosePhoto() {
+        requestReadStoragePermission();
+    }
+
+    private void startPickActiivity() {
+        Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        intent.setType("image/*");
+        startActivityForResult(intent, LOADED_PHOTO);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == LOADED_PHOTO && resultCode == RESULT_OK && data != null) {
+            Uri selectedImage = data.getData();
+            String[] filePathColumn = {MediaStore.Images.Media.DATA};
+            Cursor cursor = getContentResolver().query(selectedImage,
+                    filePathColumn, null, null, null);
+
+            if (cursor == null || cursor.getCount() < 1) {
+                return;
+            }
+
+            cursor.moveToFirst();
+            int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+            if (columnIndex < 0)
+                return;
+
+            String picturePath = cursor.getString(columnIndex);
+            cursor.close();
+
+            Bitmap photo = BitmapFactory.decodeFile(picturePath);
+            choosenPhoto.clearColorFilter();
+            choosenPhoto.setImageBitmap(photo);
+        }
+    }
+
+    private void requestReadStoragePermission() {
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, READ_STORAGE);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == READ_STORAGE && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            startPickActiivity();
+        } else {
+            Toast.makeText(this, "Добавьте разрешение", Toast.LENGTH_LONG).show();
+        }
     }
 }
