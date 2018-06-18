b7c21c2e41f1b43fed0c04bc207b14f5e5c6d4ff
Venom <antonzaritsky@gmail.com>
2018-06-18 17:23:48 +0300

    Насколько, я смог разобраться, то мне кажеться что тесты написаны верно, но вылетает с ошибкой, что метод должен иметь пустые парпметры, как так если в тестируемом методе входной параметр присутствует?

--- app/src/androidTest/java/com/example/anton/androidlibhomework/CahceInstrumentedTest.java
+++ app/src/androidTest/java/com/example/anton/androidlibhomework/CahceInstrumentedTest.java
@@ -1,5 +1,8 @@
 package com.example.anton.androidlibhomework;
 
+import android.content.Context;
+
+import com.example.anton.androidlibhomework.model.cache.RealmCache;
 import com.example.anton.androidlibhomework.model.entity.Repository;
 import com.example.anton.androidlibhomework.model.entity.User;
 import com.example.anton.androidlibhomework.model.entity.realm.RealmRepository;
@@ -10,44 +13,44 @@ import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
 
 import javax.inject.Inject;
 
 import io.reactivex.Observable;
-import io.reactivex.functions.Consumer;
 import io.reactivex.observers.TestObserver;
 import io.realm.Realm;
 
 import static junit.framework.Assert.assertEquals;
 
-//TODO MOCKITO отказывается работать в пакете с интрументтесты
-//import org.mockito.Mock;
-//import org.mockito.Mockito;
-//import org.mockito.MockitoAnnotations;
 public class CahceInstrumentedTest {
-    //    @Mock
+    private static RealmCache realmCache;
+    @Mock
     private static Realm realm;
+    @Mock
+    Context fakeContext;
     @Inject
     App app;
 
     @BeforeClass
     public static void setupClass() throws IOException {
-//        MockitoAnnotations.initMocks(this)
-        realm = Realm.getDefaultInstance();
+        realmCache = new RealmCache();
+        realm = Mockito.spy(Realm.class);
     }
 
     @AfterClass
     public static void tearDownClass() throws IOException {
-        realm.close();
     }
 
     @Before
     public void setup() {
-
+        MockitoAnnotations.initMocks(this);
+        Realm realm = Realm.getDefaultInstance();
     }
 
     @After
@@ -58,16 +61,8 @@ public class CahceInstrumentedTest {
     @Test
     public void getUser(String userName) {
         TestObserver<User> observer = new TestObserver<>();
-        Observable.create(e -> {
-            Realm realm = Realm.getDefaultInstance();
-            RealmUser realmUser = realm.where(RealmUser.class).equalTo("login", userName).findFirst();
-            if (realmUser == null) {
-                e.onError(new RuntimeException("No user in cache"));
-            } else {
-                e.onNext(new User(realmUser.getLogin(), realmUser.getAvatarUrl()));
-            }
-            e.onComplete();
-        }).subscribe((Consumer<? super Object>) observer);
+        Observable<User> userObservable = realmCache.getUser(userName);
+        userObservable.subscribe(observer);
         observer.awaitTerminalEvent();
         assertEquals(observer.values().get(0).getLogin(), userName);
     }
@@ -75,20 +70,8 @@ public class CahceInstrumentedTest {
     @Test
     public void getRepos(User user) {
         TestObserver<List<Repository>> observer = new TestObserver<>();
-        Observable.create(e -> {
-            Realm realm = Realm.getDefaultInstance();
-            RealmUser realmUser = realm.where(RealmUser.class).equalTo("login", user.getLogin()).findFirst();
-            if (realmUser == null) {
-                e.onError(new RuntimeException("No user in cache"));
-            } else {
-                List<Repository> repos = new ArrayList<>();
-                for (RealmRepository realmRepository : realmUser.getRepositories()) {
-                    repos.add(new Repository(realmRepository.getId(), realmRepository.getName()));
-                }
-                e.onNext(repos);
-            }
-            e.onComplete();
-        }).subscribe((Consumer<? super Object>) observer);
+        Observable<List<Repository>> listObservable = realmCache.getUserRepos(user);
+        listObservable.subscribe(observer);
         observer.awaitTerminalEvent();
         for (int i = 0; i < observer.values().size(); i++) {
             assertEquals(observer.values().get(0).get(i), user.getRepos().get(i));
@@ -97,11 +80,54 @@ public class CahceInstrumentedTest {
 
     @Test
     public void putUser(User user) {
+        TestObserver<User> observer = new TestObserver<>();
+        Realm realm = Realm.getDefaultInstance();
+        final RealmUser realmUser = realm.where(RealmUser.class).equalTo("login", user.getLogin()).findFirst();
+        realm.executeTransaction(innerRealm ->
+        {
+            if (realmUser == null) {
+                RealmUser newRealmUser = realm.createObject(RealmUser.class, user.getLogin());
+                newRealmUser.setAvatarUrl(user.getAvatarUrl());
+            } else {
+                realmUser.setAvatarUrl(user.getAvatarUrl());
+            }
+        });
+        realmCache.getUser(user.getLogin()).subscribe(observer);
+        observer.awaitTerminalEvent();
+        assertEquals(observer.values().get(0).getLogin(), user.getLogin());
+        realm.close();
 
     }
 
     @Test
-    public void putRepos(List<Repository> repos) {
+    public void putRepos(User user, List<Repository> repositories) {
+        TestObserver<List<Repository>> observer = new TestObserver<>();
+        RealmUser realmUser = realm.where(RealmUser.class).equalTo("login", user.getLogin()).findFirst();
+        if (realmUser == null) {
+            realm.executeTransaction(innerRealm ->
+            {
+                RealmUser newRealmUser = realm.createObject(RealmUser.class, user.getLogin());
+                newRealmUser.setAvatarUrl(user.getAvatarUrl());
+            });
+        }
 
+        realmUser = realm.where(RealmUser.class).equalTo("login", user.getLogin()).findFirst();
+
+        RealmUser finalRealmUser = realmUser;
+        realm.executeTransaction(innerRealm -> {
+            finalRealmUser.getRepositories().deleteAllFromRealm();
+            for (Repository repo : repositories) {
+                RealmRepository realmRepository = realm.createObject(RealmRepository.class, repo.getId());
+                realmRepository.setName(repo.getName());
+                finalRealmUser.getRepositories().add(realmRepository);
+            }
+
+        });
+        realmCache.getUserRepos(user).subscribe(observer);
+        observer.awaitTerminalEvent();
+        for (int i = 0; i < observer.values().size(); i++) {
+            assertEquals(observer.values().get(0).get(i), user.getRepos().get(i));
+        }
+        realm.close();
     }
 }
